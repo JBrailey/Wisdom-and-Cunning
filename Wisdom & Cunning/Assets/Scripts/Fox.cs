c86bb6bf1c6db9072f7971b716e2edd28c722ee4
Jessica Brailey <jessica.brailey@students.aie.edu.au>
2016-08-26 18:16:55 +1000

    Owl/Fox Basic Prefabs and Script. Interact Script

---
+++ Wisdom & Cunning/Assets/Scripts/Fox.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Fox : MonoBehaviour {
+
+    public float speed = 1.0f;
+    
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        CheckKeyPress();
+    }
+
+    void CheckKeyPress()
+    {
+        // Movement
+        if (Input.GetKey(KeyCode.D)) // Forward
+        {
+            Move(new Vector3(speed, 0, 0));
+        }
+        else if (Input.GetKey(KeyCode.A)) // Backward
+        {
+            Move(new Vector3(-speed, 0, 0));
+        }
+    }
+
+    void Move(Vector3 newPos)
+    {
+        transform.Translate(newPos * Time.deltaTime);
+    }
+
+    public void Interact(string interactName, GameObject interactObject)
+    {
+        // e.g if Object is called Lever
+        if (interactName.Equals("Lever"))
+        {
+            //interactObject.getComponent<Lever>.flip
+        }
+    }
+}
