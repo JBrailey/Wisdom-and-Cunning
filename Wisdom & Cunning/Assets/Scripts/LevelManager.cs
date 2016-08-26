68c80c7628806f7feee39b24188d059576a698e3
ShaneAdamsAIE <sadams45@hotmail.com>
2016-08-26 14:25:46 +1000

    Menu System
    26 August 2016
    
    Began Creating Menu System.
    - Added Start Menu, Placeholder Options Menu
    - Start Menu Contains Play Button, Options Button and Exit Button.
    - Options Menu Contains Unimplemented Settings Buttons and Return to
    Main Menu Button.
    - Start Menu Buttons all Functional.
    - Options Menu Return Button Functional.

---
+++ Wisdom & Cunning/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+    public Transform mainMenu, optionsMenu;
+
+  
+	public void LoadScene(string name)
+    {
+        SceneManager.LoadScene(name); // Loads a scene with a specified name
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit(); // Quits the game
+    }
+
+    public void OptionsMenu(bool toggle)
+    {
+        if (toggle == true) // If the options menu is toggled load the options menu and deactivate the main menu
+        {
+            optionsMenu.gameObject.SetActive(toggle);
+            mainMenu.gameObject.SetActive(false);
+        }
+        else // Otherwise deactivate the options menu and activate the main menu
+        {
+            optionsMenu.gameObject.SetActive(toggle);
+            mainMenu.gameObject.SetActive(true);
+        }
+    }
+}
