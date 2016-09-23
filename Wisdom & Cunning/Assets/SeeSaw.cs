43aeef7da4b19fec98e5fb3ba65c373eeaf6619f
Pickleflaps <brendan.mcmonagle@students.aie.edu.au>
2016-09-23 14:36:12 +1000

    more pushing

---
+++ Wisdom & Cunning/Assets/SeeSaw.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SeeSaw : MonoBehaviour
+{
+    Animator anim;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        anim.Play("Catapault");
+        anim.Play("SeeSaw");
+    }
+}
---
+++ Wisdom & Cunning/Assets/catapault_boxes.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class catapault_boxes : MonoBehaviour
+{
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+            GetComponent<Rigidbody>().AddForce(1, 1, -4000);
+    }
+}
