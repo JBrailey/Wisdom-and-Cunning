3cd1ddbeea61c5bad8a22ef60e134f2f8a045f71
Pickleflaps <brendan.mcmonagle@students.aie.edu.au>
2016-10-08 17:32:43 +1000

    see saw

---
+++ Wisdom & Cunning/Assets/SeeSaw.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SeeSaw : MonoBehaviour
+{
+
+    Animator anim;
+    public GameObject fox;
+
+    // Use this for initialization
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (fox.transform.position.x > transform.position.x)
+        {
+            anim.Play("SeeSaw");
+        }
+        else if (fox.transform.position.x < transform.position.x)
+        {
+            anim.Play("SawSee");
+        }
+
+    }
+}
