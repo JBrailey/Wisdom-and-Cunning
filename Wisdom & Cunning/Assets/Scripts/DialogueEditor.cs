208fafe09fae11d857efbd9ede84ef10fb95e137
Jessica Brailey <jessica.brailey@students.aie.edu.au>
2016-10-14 00:04:28 +1100

    Dialogue System

---
+++ Wisdom & Cunning/Assets/Scripts/DialogueEditor.cs
@@ -0,0 +1,109 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class Dialogue : System.Object
+{
+    public bool isFox;
+    public string text;
+}
+
+public class DialogueEditor : MonoBehaviour
+{
+
+    public Dialogue[] dialogue;
+    bool isShowingDialogue = false;
+    bool isWritingText = false;
+    bool isLeft;
+    int dialogueIndex = 0;
+    int length;
+
+    int textIndex = 0;
+
+    public GameObject dialogueManagerObject;
+    DialogueManager dialogueManager;
+
+    // Use this for initialization
+    void Start()
+    {
+        dialogueManager = dialogueManagerObject.GetComponent<DialogueManager>();
+        length = dialogue.Length;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isShowingDialogue)
+        {
+            if (Input.GetKeyDown(KeyCode.F))
+            {
+                // If Text is being Written, Skip to end of Text
+                if (isWritingText)
+                {
+                    textIndex = dialogue[dialogueIndex].text.Length;
+                    dialogueManager.UpdateDialogue(isLeft, dialogue[dialogueIndex].isFox, dialogue[dialogueIndex].text);
+                    isWritingText = false;
+                }
+                // If Text is done being Written, Go to next Dialogue
+                else
+                {
+                    if (dialogueIndex < length - 1)
+                    {
+                        dialogueIndex++;
+                        if (isLeft)
+                        {
+                            isLeft = false;
+                        }
+                        else
+                        {
+                            isLeft = true;
+                        }
+                        // Write Dialogue
+                        StartCoroutine(WriteDialogue());
+                    }
+                    else
+                    {
+                        dialogueManager.CloseDialogue();
+                        gameObject.SetActive(false);
+                    }
+                }               
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            StartDialogue();
+        }
+    }
+
+    public void StartDialogue()
+    {
+        //Just in case old Dialogue is on the screen
+        dialogueManager.CloseDialogue();
+
+        // Makes Dialogue Appear on Aligned to the Left
+        isShowingDialogue = true;
+        isLeft = true;
+        StartCoroutine(WriteDialogue());
+    }
+
+    IEnumerator WriteDialogue()
+    {
+        // Set Length to 0
+        textIndex = 0;
+
+        // Set Writing Text to ture
+        isWritingText = true;
+
+        // Type Text one char at a time.
+        string currentText = "";
+        Debug.Log("Text: " + currentText);
+        while (textIndex < dialogue[dialogueIndex].text.Length)
+        {
+            currentText += dialogue[dialogueIndex].text[textIndex++];
+            dialogueManager.UpdateDialogue(isLeft, dialogue[dialogueIndex].isFox, currentText);
+            yield return new WaitForSeconds(0.0225f);
+        }
+        isWritingText = false;        
+    }
+}
---
+++ Wisdom & Cunning/Assets/Scripts/DialogueManager.cs
@@ -0,0 +1,87 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class DialogueManager : MonoBehaviour {
+
+    public Sprite foxLeft, foxRight, owlLeft, owlRight;
+    public Transform leftAlignDialogue, rightAlignDialogue;
+
+    Text leftAlignText, rightAlignText;
+    Image leftAlignImage, rightAlignImage;
+
+	// Use this for initialization
+	void Start () {
+        leftAlignImage = leftAlignDialogue.GetChild(1).GetComponent<Image>();   // child 1 is Image
+        rightAlignImage = rightAlignDialogue.GetChild(1).GetComponent<Image>();
+        leftAlignText = leftAlignDialogue.GetChild(2).GetComponent<Text>();     // child 2 is text
+        rightAlignText = rightAlignDialogue.GetChild(2).GetComponent<Text>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void UpdateDialogue(bool isLeft, bool isFox, string text)
+    {
+        if (isLeft)
+        {
+            ToggleAlignment(true);
+            leftAlignImage.sprite = ReturnSprite(isLeft, isFox);
+            leftAlignText.text = text;
+        }
+        else if (!isLeft)
+        {
+            ToggleAlignment(false);
+            rightAlignImage.sprite = ReturnSprite(isLeft, isFox);
+            rightAlignText.text = text;
+        }
+    }
+
+    public void CloseDialogue()
+    {
+        leftAlignDialogue.gameObject.SetActive(false);
+        rightAlignDialogue.gameObject.SetActive(false);
+    }
+
+    void ToggleAlignment(bool isLeft)
+    {
+        if (isLeft) //Turn left on and right off
+        {
+            leftAlignDialogue.gameObject.SetActive(true);
+            rightAlignDialogue.gameObject.SetActive(false);
+        }
+        else if (!isLeft) //Turn Right on & Left Off
+        {
+            leftAlignDialogue.gameObject.SetActive(false);
+            rightAlignDialogue.gameObject.SetActive(true);
+        }
+    }
+
+    Sprite ReturnSprite(bool isLeft, bool isFox)
+    {
+        if (isLeft)
+        {
+            if (isFox)
+            {
+                return foxLeft;
+            }
+            else
+            {
+                return owlLeft;
+            }
+        }
+        else
+        {
+            if (isFox)
+            {
+                return foxRight;
+            }
+            else
+            {
+                return owlRight;
+            }
+        }
+    }
+}
