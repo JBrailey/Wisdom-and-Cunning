0e80baa00a4099e348dcf290a263370372162216
Pickleflaps <brendan.mcmonagle@students.aie.edu.au>
2016-10-24 15:17:45 +1000

    sync

--- Wisdom & Cunning/Assets/Scripts/Key.cs
+++ Wisdom & Cunning/Assets/Scripts/Key.cs
@@ -4,7 +4,11 @@ using System.Collections;
 public class Key : MonoBehaviour {
 
     public GameObject keyManager;
-	
+    AudioSource bling;
+	void Start()
+    {
+        bling = GetComponent<AudioSource>();
+    }
 	// Update is called once per frame
 	void Update () {
         transform.RotateAround(transform.position, new Vector3(0, 1, 0), 2);
@@ -12,7 +16,12 @@ public class Key : MonoBehaviour {
 
     void OnTriggerEnter(Collider col)
     {
-        keyManager.GetComponent<KeyManager>().PickUpKey(1);
-        gameObject.SetActive(false);       
+        if (col.tag == "Fox" || col.tag == "Owl")
+        {
+            bling.Play();
+            keyManager.GetComponent<KeyManager>().PickUpKey(1);
+            gameObject.SetActive(false);
+            
+        }
     }
 }
