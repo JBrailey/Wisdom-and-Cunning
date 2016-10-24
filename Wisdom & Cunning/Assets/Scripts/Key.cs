7405c95514ee1f31b4ed550abae41fbab6a3a4f5
Pickleflaps <brendan.mcmonagle@students.aie.edu.au>
2016-10-24 15:43:46 +1000

    syncing

--- Wisdom & Cunning/Assets/Scripts/Key.cs
+++ Wisdom & Cunning/Assets/Scripts/Key.cs
@@ -4,11 +4,7 @@ using System.Collections;
 public class Key : MonoBehaviour {
 
     public GameObject keyManager;
-    AudioSource bling;
-	void Start()
-    {
-        bling = GetComponent<AudioSource>();
-    }
+  
 	// Update is called once per frame
 	void Update () {
         transform.RotateAround(transform.position, new Vector3(0, 1, 0), 2);
@@ -18,7 +14,7 @@ public class Key : MonoBehaviour {
     {
         if (col.tag == "Fox" || col.tag == "Owl")
         {
-            bling.Play();
+            
             keyManager.GetComponent<KeyManager>().PickUpKey(1);
             gameObject.SetActive(false);
             
--- Wisdom & Cunning/Assets/Scripts/KeyManager.cs
+++ Wisdom & Cunning/Assets/Scripts/KeyManager.cs
@@ -6,18 +6,23 @@ public class KeyManager : MonoBehaviour {
 
     public Transform keyGUI;
 
+    AudioSource bling;
+
     int keyCount = 0;
     Text text;
     
     // Use this for initialization
 	void Start () {
         text = keyGUI.GetComponent<Text>();
+        bling = GetComponent<AudioSource>();
 	}
 
     public void PickUpKey(int keyAmount)
     {
         keyCount += keyAmount;
         UpdateKeyVisual();
+        bling.Play();
+
     }
 
     public void UseKeys(GameObject gate)
